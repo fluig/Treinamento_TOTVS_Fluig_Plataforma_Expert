561c6e841c6c909cc9d602046373412c359a3e88
Luis Felipe de Andrade Rossi <luis.rossi@sp01.local>
2020-03-05 10:36:24 -0300

    Inserção do campo TOTAL COMPRA

--- Fluig Expert/forms/Solicitação de Compras/form.js
+++ Fluig Expert/forms/Solicitação de Compras/form.js
@@ -22,3 +22,29 @@ function removedZoomItem(removedItem) {
 		reloadZoomFilterValues(inputName, filterValues);
 	}
 }
+
+function calcTotal(obj){
+	var id = $(obj)[0].id;
+
+	var index = id.split("___")[1];
+
+	var qtd = $("#txt_quantidade___"+index).val();
+    var val_un = $("#txt_valor_unitario___"+index).val();
+    
+    if(!isNaN(qtd) && !isNaN(val_un) && qtd != "" && val_un != ""){
+        $("#txt_total___"+index).val(parseInt(qtd) * parseInt(val_un));
+    }
+
+    var camposTotal = $("[name^='txt_total___']:visible");
+    var totalCamposTotal = camposTotal.length;
+    
+    var totalCompra = 0;
+    for(var i=0; i < totalCamposTotal; i++){
+    	var total = $(camposTotal)[i].value;
+    	if(!isNaN(total) && total != ""){
+            totalCompra += parseInt($(camposTotal)[i].value);    		
+    	}
+    }
+
+    $("#txt_totalCompra").val(totalCompra);    
+}
